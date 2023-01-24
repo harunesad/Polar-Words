3fc2ffdca87d362d8c82b2394185f9f4f41316f2
Harun Aşık <73292463+harunesad@users.noreply.github.com>
2023-01-24 18:24:39 +0300

    word under wave added

--- Assets/Scripts/WordsState/WordsClearState.cs
+++ Assets/Scripts/WordsState/WordsClearState.cs
@@ -11,12 +11,8 @@ public class WordsClearState : WordsBaseState
     {
         camLook = GameObject.FindObjectOfType<CamLook>().GetComponent<CamLook>();
 
-        //Debug.Log(words.ground.name);
-        //Debug.Log(words.words[0].transform.GetChild(1).name);
         if (words.words.Count == 1)
         {
-            //words.SwitchState(words.selectState);
-            Debug.Log("sss");
             index = 0;
             words.ýnputWord.text = "";
             words.ground.transform.parent.gameObject.layer = 0;
@@ -25,12 +21,9 @@ public class WordsClearState : WordsBaseState
         }
         if (words.words[index].transform.GetChild(1).gameObject == words.ground)
         {
-            Debug.Log(words.ground.name);
             words.words[index].GetComponent<Renderer>().material.color = new Color(1, 1, 1, 1);
             index++;
-            //words.words.RemoveAt(0);
             words.SwitchState(words.clearState);
-            //return;
         }
         else
         {
@@ -38,10 +31,10 @@ public class WordsClearState : WordsBaseState
             words.words[index].gameObject.layer = 3;
 
             words.words[index].transform.GetChild(1).gameObject.layer = 7;
+            words.words[index].transform.GetChild(1).GetComponent<Animator>().SetTrigger("Wave");
             words.words[index].transform.GetChild(1).DOMoveY(-0.25f, 1).OnComplete(
                 () =>
                 {
-                    //words.words[0].layer = 7;
                     Navmesh.navmesh.NavmeshSurface();
                     words.words.RemoveAt(index);
                     words.SwitchState(words.clearState);
