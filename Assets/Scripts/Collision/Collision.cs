d0ec7138a56e242c450081a52202ddd93b0a68bb
Harun Aşık <73292463+harunesad@users.noreply.github.com>
2023-03-06 22:08:25 +0300

    last bug fixed

--- Assets/Scripts/Collision/Collision.cs
+++ Assets/Scripts/Collision/Collision.cs
@@ -6,14 +6,14 @@ public class Collision : MonoBehaviour
 {
     private void OnCollisionEnter(UnityEngine.Collision collision)
     {
-        if (gameObject.layer == 7 && WordsStateManager.wordsState.currentState == WordsStateManager.wordsState.fillingState)
-        {
-            gameObject.layer = collision.gameObject.layer;
-        }
-        //if (gameObject.layer == 7)
+        //if (gameObject.layer == 7 && WordsStateManager.wordsState.currentState == WordsStateManager.wordsState.fillingState)
         //{
         //    gameObject.layer = collision.gameObject.layer;
         //}
+        if (gameObject.layer == 7)
+        {
+            gameObject.layer = collision.gameObject.layer;
+        }
         if (WordsStateManager.wordsState.finishGround == gameObject)
         {
             gameObject.layer = collision.gameObject.layer;
